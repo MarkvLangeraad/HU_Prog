ef37b4bce428c1b538f6b4e5ac7f6bbdbce3b68f
Mark van Langeraad <mark.vanlangeraad@student.hu.nl>
2018-09-19 15:34:31 +0200

    Ewa

--- pe5_4.py
+++ pe5_4.py
@@ -3,11 +3,4 @@ dagen = ('Maandag', 'Dinsdag', 'Woensdag')
 for i in dagen:
     print(i[0:2])
 
-getallen= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for x in getallen:
-    if x in [2,4,6,8,10]:
-        print(x)
-
-
-print('test')
